2035b9f6a5bf0dad46a602b67bccaa245c6406b2
kunaldhole2403-prog <kunaldhole2403@gmail.com>
2026-06-24 21:37:51 +0530

    Added

---
+++ Day 14/polymor.py
@@ -0,0 +1,16 @@
+class payment:
+    def pay(self):
+        pass
+    print("payment process started")
+class upi(payment):
+    def pay(self):
+        return "payment done by upi"
+    
+class gpay(payment):
+    def pay(self):
+        return "payment done by gpay"
+    
+u=upi()
+g=gpay()
+print(u.pay())
+print(g.pay())
